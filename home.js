c83756bc21aeb12eb8c03571dcc4033b68b72ea9
manish0444 <144358214+manish0444@users.noreply.github.com>
2023-09-29 15:43:14 +0545

    h

--- home.js
+++ home.js
@@ -1,13 +1,27 @@
-let wrapper = document.querySelector('.wrapper'),
-    signUpLink = document.querySelector('.link .signup-link'),
-    signInLink = document.querySelector('.link .signin-link');
+// Get tabLinks & tabContents
+var tabLinks = document.getElementsByClassName("tab-links"),
+  tabContents = document.getElementsByClassName("tab-contents");
 
-signUpLink.addEventListener('click', () => {
-    wrapper.classList.add('animated-signin');
-    wrapper.classList.remove('animated-signup');
-});
+// On openTab fucntion call
+function openTab(tabname) {
+  for (item of tabLinks) {
+    item.classList.remove("active-link");
+  }
+  for (item of tabContents) {
+    item.classList.remove("active-tab");
+  }
+  event.currentTarget.classList.add("active-link");
+  document.getElementById(tabname).classList.add("active-tab");
+}
 
-signInLink.addEventListener('click', () => {
-    wrapper.classList.add('animated-signup');
-    wrapper.classList.remove('animated-signin');
-});
+// Get SideMenu
+var sideMenu = document.getElementById("sidemenu");
+
+// On openTab fucntion call
+function openMenu() {
+  sideMenu.style.right = "0";
+}
+
+function closeMenu() {
+  sideMenu.style.right = "-200px";
+}
