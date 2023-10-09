d9a42cd05b3aa7f623ee142a9d36379e35494b89
manish0444 <144358214+manish0444@users.noreply.github.com>
2023-10-09 12:22:36 +0545

    h

--- process_comment.php
+++ process_comment.php
@@ -1,22 +1,35 @@
 <?php
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $name = $_POST["Name"];
-    $email = $_POST["Email"];
-    $message = $_POST["Message"];
-
-    // Your email address
-    $to = "manishbhandari0444@gmail.com"; // Replace with your personal email address
-    $subject = "New Contact Form Submission";
-    $messageBody = "Name: $name\nEmail: $email\nMessage:\n$message";
-
-    // Send the email
-    mail($to, $subject, $messageBody);
-
-    // Redirect to a thank you page
-    header("Location: thank_you.html");
-    exit;
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    // Collect form data
+    $name = $_POST['Name'];
+    $email = $_POST['Email'];
+    $message = $_POST['Message'];
+    
+    // Set recipient email address
+    $to = "manishbhandari0444@gmail.com";
+    
+    // Email subject
+    $subject = "Contact Form Submission from $name";
+    
+    // Email headers
+    $headers = "From: $email\r\n";
+    $headers .= "Reply-To: $email\r\n";
+    $headers .= "Content-type: text/html\r\n";
+    
+    // Email body
+    $body = "<h2>Contact Form Submission</h2>";
+    $body .= "<p><strong>Name:</strong> $name</p>";
+    $body .= "<p><strong>Email:</strong> $email</p>";
+    $body .= "<p><strong>Message:</strong> $message</p>";
+    
+    // Send email
+    if (mail($to, $subject, $body, $headers)) {
+        echo "Message sent successfully.";
+    } else {
+        echo "Message could not be sent.";
+    }
+} else {
+    // Handle the case where the form wasn't submitted
+    echo "Form not submitted.";
 }
-error_reporting(E_ALL);
-ini_set('display_errors', '1');
-
 ?>
--- process_registration.php
+++ process_registration.php
@@ -1,22 +1,35 @@
 <?php
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $username = $_POST["username"];
-    $email = $_POST["email"];
-    $password = $_POST["password"];
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    // Collect form data
+    $name = $_POST['Name'];
+    $email = $_POST['Email'];
+    $message = $_POST['Message'];
     
-    // Configure your email settings
-    $to = "manishbhandari0444@gmail.com"; // Your personal email address
-    $subject = "New Registration";
-    $message = "Username: $username\nEmail: $email\nPassword: $password";
-
-    // Send the email
-    mail($to, $subject, $message);
-
-    // Redirect to a thank you page
-    header("Location: index.html");
-    exit;
+    // Set recipient email address
+    $to = "manishbhandari0444@gmail.com";
+    
+    // Email subject
+    $subject = "Contact Form Submission from $name";
+    
+    // Email headers
+    $headers = "From: $email\r\n";
+    $headers .= "Reply-To: $email\r\n";
+    $headers .= "Content-type: text/html\r\n";
+    
+    // Email body
+    $body = "<h2>Contact Form Submission</h2>";
+    $body .= "<p><strong>Name:</strong> $name</p>";
+    $body .= "<p><strong>Email:</strong> $email</p>";
+    $body .= "<p><strong>Message:</strong> $[message]</p>";
+    
+    // Send email
+    if (mail($to, $subject, $body, $headers)) {
+        echo "Message sent successfully.";
+    } else {
+        echo "Message could not be sent.";
+    }
 } else {
-    // Handle invalid access or display an error message
-    echo "Invalid access!";
+    // Handle the case where the form wasn't submitted
+    echo "Form not submitted.";
 }
 ?>
